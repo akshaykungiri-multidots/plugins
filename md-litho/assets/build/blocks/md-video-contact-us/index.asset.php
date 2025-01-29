20b6271b7fd5e0319383c77c91ca084df4408af9
“Akshay <akshay.kungiri@multidots.com>
2025-01-29 17:31:06 +0530

    fix: update view js changes

--- md-litho/assets/build/blocks/md-video-contact-us/index.asset.php
+++ md-litho/assets/build/blocks/md-video-contact-us/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '67d4c75ce1d926a1a288');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b1b44df2d88b3e913073');
--- md-litho/assets/build/blocks/md-video-contact-us/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '9dc6862624e7ebd216e5');
